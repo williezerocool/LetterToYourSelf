9593212231281493f4d2af06ef966f4e8fe9c01e
Willie Suggs <klbooker09@gmail.com>
2017-05-05 01:39:09 -0500

    Add files via upload

---
+++ LetterToYourSelf.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lettertoyourself;
+
+/**
+ *
+ * @author Willie
+ */
+public class LetterToYourSelf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("+---------------------------------+\n|                              ###|\n|                              ###|\n|                              ###|\n|                                 |\n|                                 |\n|                                 |\n|                 Willie Doe      |\n|                 555 smith st    |\n|                 chicago Il 60652|\n+---------------------------------+");
+    }
+    
+}
